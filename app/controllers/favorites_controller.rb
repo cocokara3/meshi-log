a2d8dfd95ed7675fe4afcc70600bf4aef34b207f
cocokara3 <hirohiro1706.1706@gmail.com>
2026-03-10 20:55:39 +0900

    Add favorites feature

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,16 @@
+class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant.favorites.create(user: current_user)
+    redirect_to @restaurant, notice: 'お気に入りに追加しました'
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    favorite = @restaurant.favorites.find_by(user: current_user)
+    favorite.destroy
+    redirect_to @restaurant, notice: 'お気に入りを解除しました'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :restaurants
   devise_for :users
   post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+
+  resources :restaurants do
+    resource :favorite, only: [:create, :destroy]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
