f6e571c67c7d4f0d32d9b117fcf2028bb22d1302
cocokara3 <hirohiro1706.1706@gmail.com>
2026-03-11 03:06:32 +0900

    Add default image and fix image validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,8 +7,17 @@ class Restaurant < ApplicationRecord
   validates :genre, presence: true
   validates :rating, presence: true, inclusion: { in: 1..5 }
   validates :visited_at, presence: true
+  validate :image_size 
 
   def self.ransackable_attributes(auth_object = nil)
     %w[name genre rating comment visited_at]
+  end
+
+
+  private
+ def image_size
+   if image.attached? && image.blob.byte_size > 5.megabytes
+      errors.add(:image, '5MB以下にしてください')
+   end
   end
 end
