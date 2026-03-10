ec8de3ea8dbc8ce4f1f3016d2a714b99eba0eafc
cocokara3 <hirohiro1706.1706@gmail.com>
2026-03-10 23:50:37 +0900

    Add guest login, navbar, fix notice duplication

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   resources :restaurants do
     resource :favorite, only: [:create, :destroy]
   end
-
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
   root "restaurants#index"
 end
