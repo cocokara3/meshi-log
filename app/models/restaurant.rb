6199732e3a30402f4f0a827d398064510402d0bf
cocokara3 <hirohiro1706.1706@gmail.com>
2026-03-11 00:27:40 +0900

    Add validations to Restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,11 @@ class Restaurant < ApplicationRecord
   has_one_attached :image
   has_many :favorites, dependent: :destroy
 
+  validates :name, presence: true
+  validates :genre, presence: true
+  validates :rating, presence: true, inclusion: { in: 1..5 }
+  validates :visited_at, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     %w[name genre rating comment visited_at]
   end
