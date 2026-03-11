d8440b70d07d2d99e1f6fa591e6498b27ecf3967
cocokara3 <hirohiro1706.1706@gmail.com>
2026-03-11 22:09:18 +0900

    Add db:seed to render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
-
 gem install bundler
 bundle config set frozen false
 bundle update net-pop
@@ -8,3 +7,4 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+bundle exec rails db:seed
